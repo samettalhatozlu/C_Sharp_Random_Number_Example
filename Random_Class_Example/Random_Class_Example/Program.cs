605bb7cc2025e55cb761c30ad46fbedfeb5c8dd3
Samet Talha Tozlu <111143783+samettalhatozlu@users.noreply.github.com>
2022-08-14 15:53:48 +0300

    Random Number Example
    Random number example with C#

---
+++ Random_Class_Example/Random_Class_Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Class_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int sayi;
+            sayi = random.Next(1,82);
+            Console.WriteLine(sayi);
+
+            Console.Read();
+        }
+    }
+}
